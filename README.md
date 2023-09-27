81c8cc7adc09becb69f2366586a359b4be04d4b7
msleigh <msleigh@users.noreply.github.com>
2023-09-27 20:21:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### Recent posts
+
+<!-- blog starts -->
+Latest posts on [msleigh](https://msleigh.io/) (updated 2023-07-12T18:08:27+00:00):
+- [Markdown notes from Zotero](https://msleigh.io/zotero/markdown/zettelkasten/2023/07/12/markdown-notes-from-zotero.html) - 2023-07-12
+- [Text extraction from PDF (with Python), two ways](https://msleigh.io/2023/06/12/text-extraction-from-pdf-two-ways.html) - 2023-06-12
+- [Where is Python on macOS?](https://msleigh.io/python/macos/homebrew/2023/05/14/where-is-python.html) - 2023-05-14
+- [MathJax on Jekyll](https://msleigh.io/jekyll/web/2022/11/20/mathjax.html) - 2022-11-20
+- [How to get an external link icon (uses Iconify)](https://msleigh.io/jekyll/web/2022/11/19/how-external-link-icon.html) - 2022-11-19
+- [How to get a favicon](https://msleigh.io/jekyll/web/2022/11/18/how-favicon.html) - 2022-11-18
+<!-- blog ends -->
+
+<a href="https://github.com/msleigh/msleigh/actions"><img src="https://github.com/msleigh/msleigh/workflows/Build%20README/badge.svg" align="right" alt="Build README"></a>
+<a href="https://simonwillison.net/2020/Jul/10/self-updating-profile-readme/">Credit</a>
---
+++ build_readme.py
@@ -0,0 +1,53 @@
+import feedparser
+import pathlib
+import re
+
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def fetch_blog_entries():
+    entries = feedparser.parse(
+        r"https://msleigh.io/feed.xml"
+    )
+    return (
+        entries.feed.title,
+        entries.feed.link,
+        entries.feed.description,
+        entries.feed.updated,
+        [
+            {
+                "title": entry["title"],
+                "url": entry["link"].split("#")[0],
+                "published": entry["published"].split("T")[0],
+            }
+            for entry in entries.entries
+        ],
+    )
+
+
+if __name__ == "__main__":
+    root = pathlib.Path(__file__).parent.resolve()
+    readme = root / "README.md"
+    readme_contents = readme.open().read()
+    entries = fetch_blog_entries()
+    entries_md = (
+        f"Latest posts on [{entries[0]}]({entries[1]}) (updated {entries[3]}):\n"
+        + "\n".join(
+            [
+                "- [{title}]({url}) - {published}".format(**entry)
+                for entry in entries[4][:6]
+            ]
+        )
+    )
+    print(entries_md)
+    rewritten = replace_chunk(readme_contents, "blog", entries_md)
+    readme.open("w").write(rewritten)
